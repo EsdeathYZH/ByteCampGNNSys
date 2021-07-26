c55d15ba17393a2d928a364c8cd0edc0b4fd9c13
zjw1111 <774258017@qq.com>
2021-07-27 00:24:03 +0800

    add thrift idls and demo code.

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,17 +1,31 @@
-#include "glog/logging.h"
+#include "client.h"
 
 int main(int argc, char **argv) {
-  // Initialize Google’s logging library.
-  google::InitGoogleLogging(argv[0]);
-  google::SetLogDestination(google::INFO, "/tmp/log/INFO_");
-//  google::SetStderrLogging(google::INFO);
-  FLAGS_logtostderr = 1;
+    // Initialize Google’s logging library.
+    google::InitGoogleLogging(argv[0]);
+    google::SetLogDestination(google::INFO, "/tmp/log/INFO_");
+    // google::SetStderrLogging(google::INFO);
+    FLAGS_logtostderr = 1;
+    string ret;
 
-  // ...
-  LOG(INFO) << "Found " << 1 << " cookies";
-  FLAGS_logtostderr = 0;
-  LOG(INFO) << "Found " << 2 << " cookies";
-  FLAGS_logtostderr = 1;
-  LOG(INFO) << "Found " << 3 << " cookies";
+    // ...
+    LOG(INFO) << "Found " << 1 << " cookies";
+    FLAGS_logtostderr = 0;
+    LOG(INFO) << "Found " << 2 << " cookies";
+    FLAGS_logtostderr = 1;
+    LOG(INFO) << "Found " << 3 << " cookies";
+
+    string peerIP = "127.0.0.1";
+    int port = 9090;
+    socket_ = std::make_shared<att::TSocket>(peerIP, port);
+    transport_ = ::std::shared_ptr<att::TTransport>(new att::TFramedTransport(socket_));
+    protocol_ = ::std::shared_ptr<atp::TProtocol>(new atp::TBinaryProtocol(transport_));
+    client_ = std::make_shared<ByteGraph::GraphServicesClient>(protocol_);
+    transport_->open();
+    LOG(INFO) << "connected to server " << peerIP;
+
+    client_->sayHello(ret, 1234, "thrift_test");
+    LOG(INFO) << ret;
+
     return 0;
 }
--- src/server/worker.cpp
+++ src/server/worker.cpp
@@ -1,6 +1,26 @@
+#include "worker.h"
+
 #include <iostream>
 
-int main() {
-    std::cout << "hello worker\n";
+int main(int argc, char** argv) {
+    std::cout << "[Server cout] hello worker\n";
+    int port = 9090;
+    ::std::shared_ptr<GraphServicesHandler> handler(new GraphServicesHandler());
+    ::std::shared_ptr<at::server::TProcessor> processor(new GraphServicesProcessor(handler));
+
+    ::std::shared_ptr<att::TNonblockingServerSocket> serverTransport(new att::TNonblockingServerSocket(port));
+    ::std::shared_ptr<atp::TProtocolFactory> protocolFactory(new atp::TBinaryProtocolFactory());
+
+    at::server::TNonblockingServer server(processor, protocolFactory, serverTransport);
+    server.serve();
     return 0;
+}
+
+GraphServicesHandler::GraphServicesHandler() = default;
+
+void GraphServicesHandler::sayHello(std::string& _return, const int32_t workerId, const std::string& content) {
+    // Your implementation goes here
+    printf("[Server printf] sayHello\n");
+    _return = std::string("I am worker ") + std::to_string(workerId) + std::string(", I want to say: ") + content +
+              std::string(".");
 }
