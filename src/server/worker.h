eca041e7d2d3064d3bd46dc1189ef1495715320b
zjw1111 <774258017@qq.com>
2021-07-27 22:30:55 +0800

    add thrift idls

--- src/server/worker.h
+++ src/server/worker.h
@@ -19,4 +19,19 @@ class GraphServicesHandler : virtual public GraphServicesIf {
     GraphServicesHandler();
 
     void sayHello(std::string& _return, const int32_t workerId, const std::string& content);
+
+    void getFullGraphInfo(GraphInfo& _return);
+
+    void SampleBatchNodes(BatchNodes& _return, const NodeType type, const int32_t batch_size,
+                          const SampleStrategy::type strategy);
+
+    void GetNodeFeature(NodeFeature& _return, const NodeId node_id, const FeatureId feature_id);
+
+    void GetNeighborsWithFeature(std::vector<IDFeaturePair>& _return, const NodeId node_id,
+                                 const NodeType neighbor_type, const std::vector<FeatureType>& feature_types);
+
+    void SampleNeighbor(std::vector<IDNeighborPair>& _return, const int32_t batch_size, const NodeType node_type,
+                        const NodeType neighbor_type, const int32_t sample_num);
+
+    void RandomWalk(std::vector<NodeId>& _return, const int32_t batch_size, const int32_t walk_len);
 };
